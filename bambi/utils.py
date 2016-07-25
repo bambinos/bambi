5106fa4221b63f5c0cf50f44b78921d0d68d3087
tyarkoni <tyarkoni@gmail.com>
2016-07-25 10:52:55 -0400

    drop transformation wrapper

--- bambi/utils.py
+++ bambi/utils.py
@@ -5,10 +5,3 @@ def listify(obj):
         return []
     else:
         return obj if isinstance(obj, (list, tuple, type(None))) else [obj]
-
-
-def transformer(func):
-    ''' Decorator that indicates that a method acts as a data transformer. '''
-    def wrapper(self, *args, **kwargs):
-       self.transform(func.__name__, *args, **kwargs)
-    return wrapper
