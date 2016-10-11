68aecfd44fa74bee1c149a5ab8d3a5d944c791ab
tyarkoni <tyarkoni@gmail.com>
2016-10-11 16:28:50 -0500

    PyMC3 has RC on PyPI now, so install via requirements

--- setup.py
+++ setup.py
@@ -7,9 +7,7 @@ __version__ = '0.0.3'
 if len(set(('test', 'easy_install')).intersection(sys.argv)) > 0:
     import setuptools
 
-requirements = ['numpy', 'pandas', 'patsy', 'statsmodels']
-# dependency_links = ['git://github.com/pymc-devs/pymc3.git#egg=pymc3']
-dependency_links = []
+requirements = ['numpy', 'pandas', 'patsy', 'pymc3', 'statsmodels']
 
 tests_require = []
 extra_setuptools_args = {}
@@ -28,7 +26,6 @@ setup(
     url='http://github.com/bambinos/bambi',
     download_url='https://github.com/bambinos/bambi/archive/%s.tar.gz' % __version__,
     install_requires=requirements,
-    dependency_links=dependency_links,
     maintainer='Tal Yarkoni',
     maintainer_email='tyarkoni@gmail.com',
     packages=find_packages(exclude=['tests', 'test_*']),
