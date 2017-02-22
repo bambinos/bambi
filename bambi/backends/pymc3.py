9117012cc15cfd6fbd406ac783d60d6ba7572db5
tyarkoni <tyarkoni@gmail.com>
2017-02-22 11:35:51 -0600

    move plotting out of models module and into back-end specific modules

--- bambi/backends/pymc3.py
+++ bambi/backends/pymc3.py
@@ -1,15 +1,14 @@
 from .base import BackEnd
 from bambi.external.six import string_types
 import numpy as np
-import warnings
 from bambi.results import MCMCResults, PyMC3ADVIResults
 from bambi.priors import Prior
 import theano
 try:
     import pymc3 as pm
+    pymc3.model import FreeRV
 except:
-    warnings.warn("PyMC3 could not be imported. You will not be able to use "
-                  "PyMC3 as the back-end for your models.")
+    pm = None
 
 
 class PyMC3BackEnd(BackEnd):
@@ -27,6 +26,9 @@ class PyMC3BackEnd(BackEnd):
     }
 
     def __init__(self):
+        if pm is None:
+            raise ImportError("Could not import PyMC3; please make sure it's "
+                              "installed.")
         self.reset()
 
     def reset(self):
@@ -35,8 +37,6 @@ class PyMC3BackEnd(BackEnd):
         '''
         self.model = pm.Model()
         self.mu = None
-        self.dists = {}
-        self.shared_params = {}
 
     def _build_dist(self, label, dist, **kwargs):
         ''' Build and return a PyMC3 Distribution. '''
@@ -141,3 +141,50 @@ class PyMC3BackEnd(BackEnd):
             with self.model:
                 self.advi_params = pm.variational.advi(start, **kwargs)
             return PyMC3ADVIResults(self.spec, self.advi_params)
+
+    def plot_priors(self, model):
+        # Currently this only supports plotting priors for fixed effects
+        if not model.built:
+            raise ValueError("Cannot plot priors until model is built!")
+
+        with pm.Model():
+            # get priors for fixed fx, separately for each level of each predictor
+            dists = []
+            for t in model.fixed_terms.values():
+                for i,l in enumerate(t.levels):
+                    params = {k: v[i % len(v)] \
+                        if isinstance(v, np.ndarray) else v
+                        for k,v in t.prior.args.items()}
+                    dists += [getattr(pm, t.prior.name)(l, **params)]
+
+            # get priors for random effect SDs
+            for t in model.random_terms.values():
+                prior = t.prior.args['sd'].name
+                params = t.prior.args['sd'].args
+                dists += [getattr(pm, prior)(t.name+'_sd', **params)]
+
+            # add priors on Y params if applicable
+            y_prior = [(k,v) for k,v in model.y.prior.args.items()
+                if isinstance(v, Prior)]
+            if len(y_prior):
+                for p in y_prior:
+                    dists += [getattr(pm, p[1].name)('_'.join([model.y.name,
+                        p[0]]), **p[1].args)]
+
+            # make the plot!
+            p = float(len(dists))
+            fig, axes = plt.subplots(int(np.ceil(p/2)), 2,
+                figsize=(12,np.ceil(p/2)*2))
+            # in case there is only 1 row
+            if int(np.ceil(p/2))<2: axes = axes[None,:]
+            for i,d in enumerate(dists):
+                dist = d.distribution if isinstance(d, FreeRV) else d
+                samp = pd.Series(dist.random(size=1000).flatten())
+                samp.plot(kind='hist', ax=axes[divmod(i,2)[0], divmod(i,2)[1]],
+                    normed=True)
+                samp.plot(kind='kde', ax=axes[divmod(i,2)[0], divmod(i,2)[1]],
+                    color='b')
+                axes[divmod(i,2)[0], divmod(i,2)[1]].set_title(d.name)
+            fig.tight_layout()
+
+        return axes
--- bambi/models.py
+++ bambi/models.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import pymc3 as pm
-from pymc3.model import FreeRV
 import matplotlib.pyplot as plt
 from bambi.external.six import string_types
 from bambi.external.patsy import Ignore_NA
@@ -570,51 +568,8 @@ class Model(object):
             self.terms[name].prior = prior
 
     def plot(self, kind='priors'):
-        # Currently this only supports plotting priors for fixed effects
-        if not self.built:
-            raise ValueError("Cannot plot priors until model is built!")
-
-        with pm.Model():
-            # get priors for fixed fx, separately for each level of each predictor
-            dists = []
-            for t in self.fixed_terms.values():
-                for i,l in enumerate(t.levels):
-                    params = {k: v[i % len(v)] \
-                        if isinstance(v, np.ndarray) else v
-                        for k,v in t.prior.args.items()}
-                    dists += [getattr(pm, t.prior.name)(l, **params)]
-
-            # get priors for random effect SDs
-            for t in self.random_terms.values():
-                prior = t.prior.args['sd'].name
-                params = t.prior.args['sd'].args
-                dists += [getattr(pm, prior)(t.name+'_sd', **params)]
-
-            # add priors on Y params if applicable
-            y_prior = [(k,v) for k,v in self.y.prior.args.items()
-                if isinstance(v, Prior)]
-            if len(y_prior):
-                for p in y_prior:
-                    dists += [getattr(pm, p[1].name)('_'.join([self.y.name,
-                        p[0]]), **p[1].args)]
-            
-            # make the plot!
-            p = float(len(dists))
-            fig, axes = plt.subplots(int(np.ceil(p/2)), 2,
-                figsize=(12,np.ceil(p/2)*2))
-            # in case there is only 1 row
-            if int(np.ceil(p/2))<2: axes = axes[None,:]
-            for i,d in enumerate(dists):
-                dist = d.distribution if isinstance(d, FreeRV) else d
-                samp = pd.Series(dist.random(size=1000).flatten())
-                samp.plot(kind='hist', ax=axes[divmod(i,2)[0], divmod(i,2)[1]],
-                    normed=True)
-                samp.plot(kind='kde', ax=axes[divmod(i,2)[0], divmod(i,2)[1]],
-                    color='b')
-                axes[divmod(i,2)[0], divmod(i,2)[1]].set_title(d.name)
-            fig.tight_layout()
-        
-        return axes
+        if kind == 'priors':
+            self.backend.plot_priors(self)
 
     @property
     def term_names(self):
