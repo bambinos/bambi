0360695e62532ac1a5e7d2d1eda19b7e2a5a763b
tyarkoni <tyarkoni@gmail.com>
2017-03-13 12:48:43 -0500

    refactor backend classes into separate modules

--- bambi/__init__.py
+++ bambi/__init__.py
@@ -1,3 +1,13 @@
 from .models import Model
 from .priors import Prior, Family
 from .results import ModelResults
+from .backends import StanBackEnd, PyMC3BackEnd
+
+__all__ = [
+    'Model',
+    'Prior',
+    'Family',
+    'ModelResults',
+    'StanBackEnd',
+    'PyMC3BackEnd'
+]
