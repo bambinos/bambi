10a5903a61eb57c71322afe1fc995eb4c81c82fc
aloctavodia <aloctavodia@gmail.com>
2019-07-13 17:55:00 -0300

    add arviz to requirements

--- scripts/create_testenv.sh
+++ scripts/create_testenv.sh
@@ -21,7 +21,7 @@ if [[ $* != *--global* ]]; then
     fi
 fi
 
-conda install --yes pip pytest numpy matplotlib pandas patsy statsmodels
+conda install --yes pip pytest numpy matplotlib pandas patsy statsmodels arviz
 
 pip install --upgrade pip
 
