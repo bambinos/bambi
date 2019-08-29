cf85dd3521bf866e37ffa090e30d0567f121c734
Camen Piho <camen@getfreebird.com>
2019-08-28 22:39:04 -0700

    Add long description to setup.py; light linting on setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,47 @@
-import sys
-from setuptools import setup, find_packages
+import codecs
 import os
+import sys
 
-mod_dir = os.path.dirname(__file__)
-with open(os.path.join(mod_dir, 'bambi', 'version.py')) as f:
-    exec(f.read())
+from setuptools import find_packages, setup
 
-if len(set(('test', 'easy_install')).intersection(sys.argv)) > 0:
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+README_FILE = os.path.join(PROJECT_ROOT, "README.md")
+VERSION_FILE = os.path.join(PROJECT_ROOT, "bambi", "version.py")
+
+
+def get_long_description():
+    with codecs.open(README_FILE, "rt") as buff:
+        return buff.read()
+
+
+with open(VERSION_FILE) as buff:
+    exec(buff.read())
+
+if len(set(("test", "easy_install")).intersection(sys.argv)) > 0:
     import setuptools
-
-requirements = ['numpy', 'pandas', 'patsy', 'pymc3', 'statsmodels', 'arviz']
 
 tests_require = []
 extra_setuptools_args = {}
-if 'setuptools' in sys.modules:
-    tests_require.append('nose')
+if "setuptools" in sys.modules:
+    tests_require.append("nose")
     extra_setuptools_args = dict(
-        test_suite='nose.collector',
-        extras_require=dict(
-            test='nose>=0.10.1')
+        test_suite="nose.collector", extras_require=dict(test="nose>=0.10.1")
     )
 
 setup(
     name="bambi",
     version=__version__,
     description="BAyesian Model Building Interface in Python",
-    url='http://github.com/bambinos/bambi',
-    download_url='https://github.com/bambinos/bambi/archive/%s.tar.gz' % __version__,
-    install_requires=requirements,
-    maintainer='Tal Yarkoni',
-    maintainer_email='tyarkoni@gmail.com',
-    packages=find_packages(exclude=['tests', 'test_*']),
-    package_data={'bambi': ["config/*"]},
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url="http://github.com/bambinos/bambi",
+    download_url="https://github.com/bambinos/bambi/archive/%s.tar.gz" % __version__,
+    install_requires=["numpy", "pandas", "patsy", "pymc3", "statsmodels", "arviz"],
+    maintainer="Tal Yarkoni",
+    maintainer_email="tyarkoni@gmail.com",
+    packages=find_packages(exclude=["tests", "test_*"]),
+    package_data={"bambi": ["config/*"]},
     tests_require=tests_require,
-    license='MIT',
-    **extra_setuptools_args
+    license="MIT",
+    **extra_setuptools_args,
 )
