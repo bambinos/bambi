fb48a502576228f2e655771497305ec5579cffd2
Camen <camenpihor@gmail.com>
2020-07-03 11:55:57 -0700

    Add black style check on lint

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
 set -ex # fail on first error, print commands
+
+echo "Checking code style with black...."
+python -m black bambi --line-length=100  --target-version=py37 --exclude=tests/ --check
+echo "Success!"
 
 echo "Checking code style with pylint..."
 python -m pylint bambi/
