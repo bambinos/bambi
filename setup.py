0febf5da08a9caf9fd140d9df0164f25c556b466
Tomás Capretto <tomicapretto@gmail.com>
2022-03-13 05:00:41 -0300

    Clean setup.py (#468)
    * Clean unused deps in setup and change maintainer info
    
    * more consistent style
    
    * remove codecs

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import codecs
 import os
 import sys
 
@@ -11,40 +10,35 @@ REQUIREMENTS_FILE = os.path.join(PROJECT_ROOT, "requirements.txt")
 MINIMUM_PYTHON_VERSION = (3, 7, 2)
 
 
-def check_installation(rv):
-    if sys.version_info < rv:
+def check_installation():
+    if sys.version_info < MINIMUM_PYTHON_VERSION:
+        version = ".".join(str(i) for i in MINIMUM_PYTHON_VERSION)
         sys.stderr.write(
-            f"[{sys.argv[0]}] - Error: Your Python interpreter must be "
-            + f"{rv[0], rv[1], rv[2]} or greater\n"
+            f"[{sys.argv[0]}] - Error: Your Python interpreter must be {version} or greater.\n"
         )
         sys.exit(-1)
 
 
 def get_long_description():
-    with codecs.open(README_FILE, "rt") as buff:
+    with open(README_FILE, encoding="utf-8") as buff:
         return buff.read()
 
 
 def get_requirements():
-    with codecs.open(REQUIREMENTS_FILE) as buff:
+    with open(REQUIREMENTS_FILE, encoding="utf-8") as buff:
         return buff.read().splitlines()
 
 
-check_installation(MINIMUM_PYTHON_VERSION)
+def get_version():
+    with open(VERSION_FILE, encoding="utf-8") as buff:
+        exec(buff.read())  # pylint: disable=exec-used
+    return vars()["__version__"]
 
-with open(VERSION_FILE) as buff:
-    exec(buff.read())
 
-if len(set(("test", "easy_install")).intersection(sys.argv)) > 0:
-    import setuptools
+check_installation()
 
-tests_require = []
-extra_setuptools_args = {}
-if "setuptools" in sys.modules:
-    tests_require.append("nose")
-    extra_setuptools_args = dict(
-        test_suite="nose.collector", extras_require=dict(test="nose>=0.10.1")
-    )
+__version__ = get_version()
+
 
 setup(
     name="bambi",
@@ -55,11 +49,9 @@ setup(
     url="http://github.com/bambinos/bambi",
     download_url="https://github.com/bambinos/bambi/archive/%s.tar.gz" % __version__,
     install_requires=get_requirements(),
-    maintainer="Tal Yarkoni",
-    maintainer_email="tyarkoni@gmail.com",
+    maintainer="Tomas Capretto",
+    maintainer_email="tomicapretto@gmail.com",
     packages=find_packages(exclude=["tests", "test_*"]),
     package_data={"bambi": ["priors/config/*"]},
-    tests_require=tests_require,
     license="MIT",
-    **extra_setuptools_args,
 )
