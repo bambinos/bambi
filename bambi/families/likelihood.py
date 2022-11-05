55eab70f6babe3ecb905efefd61d8d15af5d7ec8
Tomas Capretto <tomicapretto@gmail.com>
2022-11-05 18:03:44 -0300

    Remove prior information from Family

--- bambi/families/likelihood.py
+++ bambi/families/likelihood.py
@@ -4,7 +4,7 @@ from bambi.priors import Prior
 from bambi.utils import multilinify, spacify
 
 
-DistSettings = namedtuple("DistSettings", ["params", "parent", "args"])
+DistSettings = namedtuple("DistSettings", ["params", "parent"])
 
 DISTRIBUTIONS = {
     "Bernoulli": DistSettings(params=("p",), parent="p"),
@@ -27,32 +27,29 @@ class Likelihood:
     """Representation of a Likelihood function for a Bambi model.
 
     Notes:
-    * ``parent`` must not be in ``kwargs``.
+    * ``parent`` must be in ``params``
     * ``parent`` is inferred from the ``name`` if it is a known name
 
     Parameters
     ----------
     name : str
         Name of the likelihood function. Must be a valid PyMC distribution name.
+    params : Sequence[str]
+        The name of the parameters the likelihood function accepts.
     parent : str
         Optional specification of the name of the mean parameter in the likelihood.
         This is the parameter whose transformation is modeled by the linear predictor.
     dist : pymc.distributions.distribution.DistributionMeta
         Optional custom PyMC distribution that will be used to compute the likelihood.
-    kwargs :
-        Keyword arguments that indicate prior distributions for auxiliary parameters in the
-        likelihood.
     """
 
     DISTRIBUTIONS = DISTRIBUTIONS
 
-    def __init__(self, name, params=None, parent=None, dist=None, **kwargs):
+    def __init__(self, name, params=None, parent=None, dist=None):
         self.name = name
         self.params = params
         self.parent = parent
-        self.priors = self._check_priors(kwargs)
         self.dist = dist
-
 
     @property
     def params(self):
@@ -86,49 +83,50 @@ class Likelihood:
             raise ValueError(f"'{value}' must be one of {self.params}")
         self._parent = value
 
-    def _check_priors(self, priors):
-        args = [param for param in self.params if param != self.parent]
-        if not args:
-            args = None
-
-        # The function requires priors but none were passed
-        if priors == {} and args is not None:
-            raise ValueError(f"'{self.name}' requires priors for the parameters {args}.")
-
-        # The function does not require priors, but at least one was passed
-        if priors != {} and args is None:
-            raise ValueError(f"'{self.name}' does not require any additional priors.")
-
-        # The function requires priors, priors were passed, but they differ from the required
-        if priors and args:
-            difference = set(args) - set(priors)
-            if len(difference):
-                raise ValueError(f"'{self.name}' misses priors for the parameters {difference}")
-
-            # And check priors passed are in fact of class Prior
-            check_all_are_priors(priors)
-
-        return priors
-
     def __str__(self):
-        args = [f"name: {self.name}", f"parent: {self.parent}", f"priors: {self.priors}"]
+        args = [("name", self.name), ("params", self.params), ("parent", self.parent)]
+        args = [f"{arg[0]}: {arg[1]}" for arg in args]
         return f"{self.__class__.__name__}({spacify(multilinify(args))}\n)"
 
     def __repr__(self):
         return self.__str__()
 
 
-def check_all_are_priors(priors):
-    """Checks if values in the supplied dictionary are all valid prior objects
+# def _check_priors(self, priors):
+#     args = [param for param in self.params if param != self.parent]
+#     if not args:
+#         args = None
 
-    An object is a valid prior if
-    * It is an instance of bambi.priors.Prior
-    * It is a number
+#     # The function requires priors but none were passed
+#     if priors == {} and args is not None:
+#         raise ValueError(f"'{self.name}' requires priors for the parameters {args}.")
 
-    Parameters
-    ----------
-    priors: dict
-        A dictionary whose values are tested to be valid priors
-    """
-    if any(not isinstance(prior, (Prior, int, float)) for prior in priors.values()):
-        raise ValueError("Prior distributions must be a 'Prior' instance or a numeric value")
+#     # The function does not require priors, but at least one was passed
+#     if priors != {} and args is None:
+#         raise ValueError(f"'{self.name}' does not require any additional priors.")
+
+#     # The function requires priors, priors were passed, but they differ from the required
+#     if priors and args:
+#         difference = set(args) - set(priors)
+#         if len(difference):
+#             raise ValueError(f"'{self.name}' misses priors for the parameters {difference}")
+
+#         # And check priors passed are in fact of class Prior
+#         check_all_are_priors(priors)
+
+#     return priors
+
+# def check_all_are_priors(priors):
+#     """Checks if values in the supplied dictionary are all valid prior objects
+
+#     An object is a valid prior if
+#     * It is an instance of bambi.priors.Prior
+#     * It is a number
+
+#     Parameters
+#     ----------
+#     priors: dict
+#         A dictionary whose values are tested to be valid priors
+#     """
+#     if any(not isinstance(prior, (Prior, int, float)) for prior in priors.values()):
+#         raise ValueError("Prior distributions must be a 'Prior' instance or a numeric value")
