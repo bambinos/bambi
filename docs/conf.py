f0bda3566a4b105fdecb75b5395ada8b923c7c12
Tomas Capretto <tomicapretto@gmail.com>
2022-12-22 00:13:09 -0300

    remove smv options

--- docs/conf.py
+++ docs/conf.py
@@ -130,18 +130,6 @@ html_static_path = ["_static"]
 #
 # html_sidebars = {}
 
-# Whitelist pattern for remotes (set to None to use local branches only)
-smv_remote_whitelist = r"^origin$"
-
-# Whitelist pattern for branches (set to None to ignore all branches)
-smv_branch_whitelist = r"^main$"
-
-# Tags are released
-smv_released_pattern = r"^refs/tags/.*$"
-
-# Tags like 0.0.1, 0.5.1 are kept
-# smv_tag_whitelist = r'^\d+\.\d+\.\d+S'
-
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
