bee8b76115308462e129e2e3b264ba98e2135312
Tomas Capretto <tomicapretto@gmail.com>
2023-01-28 18:10:26 -0300

    Take version from a file. Install optional deps from local as well

--- bambi/version.py
+++ bambi/version.py
@@ -1,1 +1,12 @@
-__version__ = "0.9.3"
+import os
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+
+
+def read_version():
+    VERSION_FILE = os.path.join(HERE, "VERSION.txt")
+    with open(VERSION_FILE, encoding="utf-8") as buff:
+        return buff.read()
+
+
+__version__ = read_version()
