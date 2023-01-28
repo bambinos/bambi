aaed1ff12240925065a3725f2f9ae12ed3f6d0d6
Tomas Capretto <tomicapretto@gmail.com>
2023-01-28 18:39:11 -0300

    lint

--- bambi/version.py
+++ bambi/version.py
@@ -1,11 +1,11 @@
 import os
 
-HERE = os.path.dirname(os.path.realpath(__file__))
+here = os.path.dirname(os.path.realpath(__file__))
 
 
 def read_version():
-    VERSION_FILE = os.path.join(HERE, "VERSION.txt")
-    with open(VERSION_FILE, encoding="utf-8") as buff:
+    version_file = os.path.join(here, "VERSION.txt")
+    with open(version_file, encoding="utf-8") as buff:
         return buff.read()
 
 
