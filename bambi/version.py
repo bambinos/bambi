7f5fa58fe70f513a66d3895268bf73ef931ca6ed
Tomás Capretto <tomicapretto@gmail.com>
2023-02-09 22:45:18 -0300

    Add extra index url to pypi test (#641)

--- bambi/version.py
+++ bambi/version.py
@@ -4,9 +4,9 @@ here = os.path.dirname(os.path.realpath(__file__))
 
 
 def read_version():
-    version_file = os.path.join(here, "VERSION.txt")
+    version_file = os.path.join(here, "version.txt")
     with open(version_file, encoding="utf-8") as buff:
-        return buff.read()
+        return buff.read().splitlines()[0]
 
 
 __version__ = read_version()
