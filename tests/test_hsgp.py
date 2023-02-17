bd3ce95698e5efe398830592c403dba2dd7e4b7c
Tomas Capretto <tomicapretto@gmail.com>
2023-02-17 00:26:06 -0300

    [no ci] latest ideas

--- tests/test_hsgp.py
+++ tests/test_hsgp.py
@@ -2,3 +2,5 @@
 # Test dictionary setting partial priors (should fail)
 # Test hsgp contribution appears in the inference data object after predicting
 # for out of sample data
+# Test/check coordinate names for the posterior predictive samples and the observed data
+# I'm thinking we need to bring back dimension names of the responses
