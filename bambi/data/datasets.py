f5e93aec04f47a7f5609afabbb7aa4cdc70ab6df
Tomas Capretto <tomicapretto@gmail.com>
2023-04-18 09:25:40 -0300

    Add mtcars as a dataset. Bump PyMC to 5.3.0

--- bambi/data/datasets.py
+++ bambi/data/datasets.py
@@ -141,7 +141,28 @@ information.
 - count: count of total rental bikes including both casual and registered
 
 Original source: https://archive.ics.uci.edu/ml/datasets/bike+sharing+dataset
-.""",
+""",
+    ),
+    "mtcars": FileMetadata(
+        filename="mtcars.csv",
+        url="https://figshare.com/ndownloader/files/40208785",
+        checksum="c802190c43e02246da9c6c9c3f13a58f076cc6b77922f4d9766a3c6bdb1b52bd",
+        description="""
+The data was extracted from the 1974 Motor Trend US magazine, and comprises fuel consumption and 10
+aspects of automobile design and performance for 32 automobiles (1973--74 models). 
+The following is a description of the variables:
+
+* mpg: Miles/(US) gallon
+* cyl: Number of cylinders
+* disp: Displacement (cu.in.)
+* hp: Gross horsepower
+* drat: Rear axle ratio
+* wt: Weight (1000 lbs)
+* qsec: 1/4 mile time
+* vs: Engine (0 = V-shaped, 1 = straight)
+* am: Transmission (0 = automatic, 1 = manual)
+* gear: Number of forward gears
+""",
     ),
 }
 
