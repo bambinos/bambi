d02fb1455f318002afeaa0a4593814a29098f826
GStechschulte <stechschulteg@gmail.com>
2023-05-18 15:57:01 +0200

    CreateData class added to __init__.py

--- bambi/plots/__init__.py
+++ bambi/plots/__init__.py
@@ -1,3 +1,5 @@
 from .plot_cap import create_cap_data, plot_cap
+from .plot_comparisons import plot_comparison
+from .utils import CreateData
 
-__all__ = ["create_cap_data", "plot_cap"]
+__all__ = ["create_cap_data", "plot_cap", "plot_comparison", "CreateData"]
