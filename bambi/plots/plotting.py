bde547069dc7782d6892ee4e6c7eaa2f66e9bd21
GStechschulte <stechschulteg@gmail.com>
2023-05-23 07:59:24 +0200

    module for plot kinds based on response

---
+++ bambi/plots/plotting.py
@@ -0,0 +1,18 @@
+
+
+# TO DO: instead of str: str, it should be bmb.response_distribution 
+# and Callable
+pps_plot_kinds = {
+    "Bernoulli": "bar",
+    "Binomial": "bar",
+    "Beta": "line",
+    "Exponential": "line",
+    "Gamma": "line",
+    "Normal": "line",
+    "NegativeBinomial": "bar",
+    "Poisson": "bar",
+    "StudentT": "line",
+    "VonMises": "line",
+    "InverseGaussian": "line",
+    "Categorical": "bar"
+}
