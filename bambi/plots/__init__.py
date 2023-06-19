64b817b940c7c70fd4abba9e42b830845002b3b1
GStechschulte <stechschulteg@gmail.com>
2023-06-19 20:33:15 +0200

    add predictions as sub-package

--- bambi/plots/__init__.py
+++ bambi/plots/__init__.py
@@ -1,12 +1,16 @@
 from .create_data import create_cap_data, create_comparisons_data
-from .effects import comparisons
+from .effects import comparisons, predictions
 from .plotting import plot_cap, plot_comparison
 from .plot_types import plot_numeric, plot_categoric
 
 
 __all__ = [
-    "create_cap_data", "create_comparisons_data",
+    "create_cap_data",
+    "create_comparisons_data",
     "comparisons",
-    "plot_cap", "plot_comparison",
-    "plot_numeric", "plot_categoric"
+    "predictions",
+    "plot_cap",
+    "plot_comparison",
+    "plot_numeric",
+    "plot_categoric",
 ]
