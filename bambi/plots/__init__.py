20998dac74ab912079194a88cc050b514cc87db6
GStechschulte <stechschulteg@gmail.com>
2023-07-02 14:05:25 +0200

    remove imports that users should not have access to

--- bambi/plots/__init__.py
+++ bambi/plots/__init__.py
@@ -1,16 +1,5 @@
-from bambi.plots.create_data import create_cap_data, create_comparisons_data
 from bambi.plots.effects import comparisons, predictions
 from bambi.plots.plotting import plot_cap, plot_comparison
-from bambi.plots.plot_types import plot_numeric, plot_categoric
 
 
-__all__ = [
-    "create_cap_data",
-    "create_comparisons_data",
-    "comparisons",
-    "predictions",
-    "plot_cap",
-    "plot_comparison",
-    "plot_numeric",
-    "plot_categoric",
-]
+__all__ = ["comparisons", "predictions", "plot_cap", "plot_comparison"]
