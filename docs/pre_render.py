ae00f268b63bb2043747d2b55e29af901bdb0643
Tomás Capretto <tomicapretto@gmail.com>
2023-08-26 11:09:09 -0300

    Migrate documentation to Quarto (#712)

---
+++ docs/pre_render.py
@@ -0,0 +1,15 @@
+import shutil
+
+# Copy CHANGELOG file to 'docs'
+shutil.copyfile("../CHANGELOG.md", "CHANGELOG.md")
+
+# Remove TOC from it
+lines = """---
+toc: false
+pagetitle: "Changelog"
+---
+"""
+with open("CHANGELOG.md", "r+") as file: 
+    file_data = file.read() 
+    file.seek(0, 0) 
+    file.write(lines + "\n" + file_data) 
