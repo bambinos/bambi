bd2405ac4740017904b7561d3e7d406504ee0a03
GStechschulte <stechschulteg@gmail.com>
2023-11-28 14:20:34 +0100

    remove functions that have been deleted from utils.py

--- bambi/interpret/logs.py
+++ bambi/interpret/logs.py
@@ -22,20 +22,7 @@ def log_interpret_defaults(func):
         arg_name = None
         covariate_name = None
 
-        if func.__name__ in ["set_default_values", "make_group_panel_values"]:
-            data_dict = kwargs.get("data_dict", args[1])
-            keys_before = list(data_dict.keys())
-            keys_after = list(func(*args, **kwargs).keys())
-            covariate_name = ", ".join([key for key in keys_after if key not in keys_before])
-
-            if len(covariate_name) > 0:
-                arg_name = "unspecified" if func.__name__ == "set_default_values" else "group/panel"
-
-        elif func.__name__ == "make_main_values":
-            covariate_name = args[1]
-            arg_name = "main"
-
-        elif func.__name__ == "set_default_variable_values":
+        if func.__name__ == "set_default_variable_values":
             variables = {"comparisons": "contrast", "slopes": "wrt"}
             arg_name = variables.get(args[0].kind)
             covariate_name = args[0].name
