6e5b192be577c46679bb7f934be6564f01639f39
Gabriel Stechschulte <63432018+GStechschulte@users.noreply.github.com>
2026-03-17 19:50:11 +0100

    Update bambi/interpret/effects.py
    Co-authored-by: Tomás Capretto <tomicapretto@gmail.com>

--- bambi/interpret/effects.py
+++ bambi/interpret/effects.py
@@ -787,9 +787,9 @@ def slopes(
         'eydx' - unit change in wrt associated with a percent change in response.
         'dyex' - percent change in wrt associated with a unit change in response.
     target : str
-        Which quantity to extract. `mean` (default) for the posterior of the parent
-        parameter (e.g. `mu`). Pass the response variable name (e.g. `mpg`) for posterior
-        predictive samples. Pass a distributional component name (e.g. `sigma`) for the
+        Which quantity to extract. `"mean"` (default) for the posterior of the parent
+        parameter (e.g. `"mu"`). Pass the response variable name (e.g. `"mpg"`) for posterior
+        predictive samples. Pass a distributional component name (e.g. `"sigma"`) for the
         posterior of that component.
     use_hdi : bool
         Whether to use highest density interval. Default is True.
